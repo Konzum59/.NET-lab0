cd2fca0bd612fb2686f47b63a64b66d69ca99fc8
student <student@L22KOMP91>
2025-03-05 18:10:26 +0100

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        
+        
+        static void Main(string[] args)
+        {
+            FizzBuzz count = new FizzBuzz();
+            count.Display();
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
+class FizzBuzz
+{
+    public int topLimit = 20;
+
+    public void Display()
+    {
+        for (int i = 0; i < topLimit; ++i)
+        {
+            if (i % 3 == 0 && i % 5 == 0)
+            {
+                Console.WriteLine("FizzBuzz");
+            }
+            else if (i % 3 == 0)
+            {
+                Console.WriteLine("Fizz");
+            }
+            else if (i % 5 == 0)
+            {
+                Console.WriteLine("Buzz");
+            }
+            else
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
